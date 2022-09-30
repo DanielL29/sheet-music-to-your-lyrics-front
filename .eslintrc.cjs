25d7edaf214cd4de90c9e82ca176e7a93bcf750c
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-29 23:02:32 -0300

    chore: initial structure of react with vite

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'no-console': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [0],
+    'import/extensions': 'off',
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="App">
+      sheet music to your lyrics
+    </div>
+  );
+}
