9d0eb4fe2938405c2664def175e9441c83ece7ac
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-05 07:19:53 -0300

    feat: music page

---
+++ src/pages/index.ts
@@ -0,0 +1,13 @@
+import Home from './Home/Home';
+import Music from './Music/Music';
+import SignIn from './Auth/SignIn';
+import SignUp from './Auth/SignUp';
+
+const Pages = {
+  Home,
+  Music,
+  SignIn,
+  SignUp,
+};
+
+export default Pages;
