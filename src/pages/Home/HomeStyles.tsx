47d69b76fa612b556b68f759716eecbe28c7dd9d
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-06 02:27:32 -0300

    feat: home page

--- src/pages/Home/HomeStyles.tsx
+++ src/pages/Home/HomeStyles.tsx
@@ -1,11 +1,56 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
   width: 1200px;
+  margin: 0 auto;
+  
+  > div {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    align-items: center;
+    margin-top: 15px;
+  }
+
+  .categories {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .card {
+    width: 180px;
+    height: 180px;
+    border-radius: 5px;
+    background-color: #666;
+    cursor: pointer;
+    transition: all 300ms ease-in-out;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 5px;
+    box-shadow: 3px 3px 8px 0px rgba(150,150,150,0.75);
+  }
+
+  .card:hover {
+    transform: scale(1.1);
+  }
+
+  h1 {
+    font-weight: 600;
+    color: #15c7cf;
+    font-size: 23px;
+    margin-top: 100px;
+  }
+
+  h2 {
+    font-weight: 400;
+    font-size: 18px;
+    margin: 15px 0;
+  }
 `;
 
 const HomeWrapper = {
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,12 +2,14 @@ import Home from './Home/Home';
 import Music from './Music/Music';
 import SignIn from './Auth/SignIn';
 import SignUp from './Auth/SignUp';
+import Category from './Category/Category';
 
 const Pages = {
   Home,
   Music,
   SignIn,
   SignUp,
+  Category,
 };
 
 export default Pages;
