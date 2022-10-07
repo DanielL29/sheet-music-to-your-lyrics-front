86466bce13bfd262552719ee2f592228fb669771
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-07 03:15:28 -0300

    feat: add new routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -14,6 +14,8 @@ export default function Router() {
       <Route path={isLogged ? '/:category' : '*'} element={isLogged ? <Pages.Category /> : <Navigate to="/" replace />} />
       <Route path={isLogged ? '/:category/:author' : '*'} element={isLogged ? <Pages.Author /> : <Navigate to="/" replace />} />
       <Route path={isLogged ? '/:category/:author/:musicName' : '*'} element={isLogged ? <Pages.Music /> : <Navigate to="/" replace />} />
+      <Route path={isLogged ? '/musics' : '*'} element={isLogged ? <Pages.Musics /> : <Navigate to="/" replace />} />
+      <Route path={isLogged ? '/authors' : '*'} element={isLogged ? <Pages.Authors /> : <Navigate to="/" replace />} />
     </Routes>
   );
 }
