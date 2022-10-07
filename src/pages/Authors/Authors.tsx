0a1121d9aaba8a8f86189a16671b3de979485b72
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-07 03:47:31 -0300

    feat: authors page

--- src/pages/Authors/Authors.tsx
+++ src/pages/Authors/Authors.tsx
@@ -1,15 +1,31 @@
+import { useNavigate } from 'react-router-dom';
 import Components from '../../components';
+import hooks from '../../hooks';
 import AuthorsWrapper from './AuthorsStyle';
 
 export default function Authors() {
+  const { authors } = hooks.useAuthors();
+  const navigate = useNavigate();
+
   return (
     <AuthorsWrapper.Container>
       <Components.BreadCrumbs currentPage="Todos os Artistas" />
       <Components.PageTitle title="Todos os Artistas" />
       <div>
-        <div className="author">
-          asda
-        </div>
+        {authors?.map((author) => (
+          <div className="author">
+            <img src={author.imageUrl} alt="author" />
+            <div>
+              <h2 onClick={
+                () => navigate(`/${author.categories.name}/${author.name}`)
+              }
+              >
+                {author.name}
+              </h2>
+              <h3>{author.categories.name}</h3>
+            </div>
+          </div>
+        ))}
       </div>
     </AuthorsWrapper.Container>
   );
