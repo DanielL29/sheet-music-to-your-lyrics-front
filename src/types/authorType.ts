fee4f9b762bb25204697e983d331cfdd2ef03e1e
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-07 03:46:51 -0300

    feat: authors type

--- src/types/authorType.ts
+++ src/types/authorType.ts
@@ -2,3 +2,11 @@ export interface IAuthorByCategory {
   name: string
   imageUrl: string
 }
+
+export interface IAuthors {
+  name: string
+  imageUrl: string
+  categories: {
+    name: string
+  }
+}
