7d6f28cf6913d0785a0e776d3d5b28e73dbc119b
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-08 01:12:00 -0300

    feat: restricting functions or elements in components to contributor user

--- src/components/SnackbarAlert/SnackbarAlert.tsx
+++ src/components/SnackbarAlert/SnackbarAlert.tsx
@@ -3,19 +3,22 @@ import { Snackbar, Alert } from '@mui/material';
 interface ISnackbarAlert {
   error: any
   openAlert: any
+  success?: boolean
   closeAlert: () => void
 }
 
-export default function SnackbarAlert({ error, openAlert, closeAlert }: ISnackbarAlert) {
+export default function SnackbarAlert({
+  error, openAlert, success, closeAlert,
+}: ISnackbarAlert) {
   return (
     <Snackbar
-      anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+      anchorOrigin={{ horizontal: success ? 'center' : 'left', vertical: 'top' }}
       open={openAlert}
-      autoHideDuration={10000}
+      autoHideDuration={success ? 60000 : 10000}
       onClose={() => (closeAlert())}
     >
       <Alert
-        severity="error"
+        severity={success ? 'success' : 'error'}
         sx={{ width: '100%' }}
         onClose={() => (closeAlert())}
       >
