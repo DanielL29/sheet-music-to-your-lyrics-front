b0266f79dc62da9aedd1aca9e0fe47ea304e72ef
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-10 17:28:01 -0300

    feat: auto logout

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useUser from './useUser';
 
 export default function useAsync<T>(handler: any, immediate: boolean = true) {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const { setCurrentUser } = useUser();
 
   const act = (...args: any) => {
     setLoading(true);
@@ -13,6 +18,13 @@ export default function useAsync<T>(handler: any, immediate: boolean = true) {
       setLoading(false);
     }).catch((promiseError: any) => {
       console.log(promiseError);
+
+      if (promiseError.response.data === 'jwt expired') {
+        localStorage.removeItem('userLocal');
+        setCurrentUser(null);
+        navigate('/');
+      }
+
       setError(promiseError.response.data);
       setLoading(false);
     });
